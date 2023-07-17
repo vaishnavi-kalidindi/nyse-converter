9f6f6c9bff0bcb6704ab6484a3221101e1296beb
vaishnavi-kalidindi <vaishnavikalidindi23@gmail.com>
2023-07-17 18:58:18 +0530

    Added Environment Variables

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
  
 import pandas as pd
 from dask import dataframe as dd
+import os
 
-
+#'data/nyse_all/nyse_data/NYSE*.txt.gz'
+#'data/nyse_all/nyse_json/part-*.json.gz'
 
 
 def main():
+    src_base_dir=os.environ['SRC_BASE_DIR']
+    tgt_base_dir=os.environ['TGT_BASE_DIR']
     print('File Format Conversion Started')
-    dn=dd.read_csv('data/nyse_all/nyse_data/NYSE*.txt.gz',
+    dn=dd.read_csv(f'{src_base_dir}/NYSE*.txt.gz',
         
         blocksize=None,
         names=['ticker', 'trade_date', 'open_price', 'low_price', 'high_price', 'close_price', 'volume']
     )
     print('DataFrame is created and written in JSON Format')
     dn.to_json( 
-           f'data/nyse_all/nyse_json/part-*.json.gz',
+           f'{tgt_base_dir}/part-*.json.gz',
            orient='records',
            lines=True,
            compression='gzip',
